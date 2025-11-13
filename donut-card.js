9f587af77f07baad7fe127940e9eeb3078a16b9a
LodeBo <l.bongaerts@telenet.be>
2025-11-13 08:27:28 +0100

    Update donut-card.js

--- donut-card.js
+++ donut-card.js
@@ -4,7 +4,7 @@
 
 (() => {
   const TAG = "donut-card";
-  const VERSION = "1.0.5";
+  const VERSION = "1.0.6";
 
   // 🔹 ALIAS-NORMALISATIE (enige toevoeging buiten de classes)
   function normalizeConfig(cfg = {}) {
